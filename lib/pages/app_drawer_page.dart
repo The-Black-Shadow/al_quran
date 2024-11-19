06c2509d85b249ab9164a6413e6f52402bcb186f
the-black-shadow <mehedihasan.cse18@gmail.com>
2024-11-19 13:44:20 +0600

    style listtile in homepage appdrawer

--- lib/pages/app_drawer_page.dart
+++ lib/pages/app_drawer_page.dart
@@ -2,7 +2,6 @@ import 'package:al_quran/modules/surah_list.dart';
 import 'package:al_quran/pages/audio_player.dart';
 import 'package:flutter/material.dart';
 
-
 class AppDrawerPage extends StatelessWidget {
   const AppDrawerPage({super.key});
 
@@ -16,20 +15,52 @@ class AppDrawerPage extends StatelessWidget {
         itemCount: surahList.length,
         itemBuilder: (context, index) {
           final surah = surahList[index];
-          return ListTile(
-            title: Text(surah['title']),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AudioPlayerPage(
-                    title: surah['title'],
-                    filePath: surah['file'],
-                    chapters: surah['chapters'],
-                  ),
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              leading: CircleAvatar(
+                backgroundColor: Colors.blueAccent,
+                child: Text(
+                  (index + 1).toString(),
+                  style: const TextStyle(color: Colors.white),
                 ),
-              );
-            },
+              ),
+              title: Text(
+                surah['title'],
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: const Text(
+                'Tap to play',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.blueAccent,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AudioPlayerPage(
+                      title: surah['title'],
+                      filePath: surah['file'],
+                      chapters: surah['chapters'],
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
